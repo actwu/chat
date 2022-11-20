12fb98bd24a3a00c052c157bef914e6cefe9f976
Laucsap <110190482+actwu@users.noreply.github.com>
2022-11-20 12:58:53 +0800

    Add files via upload

---
+++ sets/config.js
@@ -0,0 +1,20 @@
+
+id = true;
+id < !true ? 'continue' : 'run'
+var imports = [
+
+
+screens = document.querySelector('#screens'),
+temp = "iframe",
+t = type = 'https://',
+subname = 'atstem',
+d= dal = subname + ".adalo.com",
+ics = '0853925',
+deploy_type = '_test',
+visibility = '_public',
+id = key = `?`,
+
+setout='class="fullscreen layer1"',
+setone='class="fullscreen layer1 white2-bg dark-txt center"',
+]
+
---
+++ sets/engine.js
@@ -0,0 +1,16 @@
+
+    function start_media() {
+       read_reference = t + d + key;
+      runtime =  `<${temp} src="${read_reference}" ${setout}></${temp}>`
+
+     screens.innerHTML = runtime;
+
+   }
+   
+    setTimeout(() => {
+   start_media()
+  }, 1000);
+
+  const deploy = [key = [`/testchatshehe${deploy_type}${visibility}-${ics}`]]
+
+  console.table(deploy)
